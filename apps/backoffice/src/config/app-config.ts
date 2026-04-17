22c41e3c6a313aea11020d8c88af71c30650de34
codeguru0x <codeguru0x@gmail.com>
2026-04-17 22:01:23 +0700

    Update version in app-config for backoffice application from "20260409" to "20260417" to reflect the latest release.

--- apps/backoffice/src/config/app-config.ts
+++ apps/backoffice/src/config/app-config.ts
@@ -2,7 +2,7 @@ const currentYear = new Date().getFullYear();
 
 export const APP_CONFIG = {
   name: "Megawin",
-  version: "20260409",
+  version: "20260417",
   copyright: `© ${currentYear} - Megawin`,
   meta: {
     title: "Megawin Backoffice",
